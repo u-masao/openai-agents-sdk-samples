693f03c369fbb4ea7105dee47f4954d98f4080ad
u-masao <4973920+u-masao@users.noreply.github.com>
2025-03-24 20:06:55 +0900

    add debug output

--- src/handoffs.py
+++ src/handoffs.py
@@ -1,30 +1,29 @@
-from agents import Agent, InputGuardrail,GuardrailFunctionOutput, Runner
-from pydantic import BaseModel
 import asyncio
+from dataclasses import asdict
+
+from agents import Agent, GuardrailFunctionOutput, InputGuardrail, Runner
+from pydantic import BaseModel
+
 
 class HomeworkOutput(BaseModel):
     is_homework: bool
     reasoning: str
 
+
 guardrail_agent = Agent(
-    name="Guardrail check",
-    # instructions="Check if the user is asking about homework.",
+    name="ガードレールチェック",
     instructions="ユーザーが宿題について質問しているのかチェックして。",
     output_type=HomeworkOutput,
 )
 
 math_tutor_agent = Agent(
-    name="Math Tutor",
+    name="数学の家庭教師",
     handoff_description="数学の専門家のエージェント",
-    # handoff_description="Specialist agent for math questions",
-    # instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
     instructions="数学の問題について回答を与えて。回答の理由を例を交えてステップバイステップで示して",
 )
 
 history_tutor_agent = Agent(
-    name="History Tutor",
-    #handoff_description="Specialist agent for historical questions",
-    #instructions="You provide assistance with historical queries. Explain important events and context clearly.",
+    name="歴史の家庭教師",
     handoff_description="歴史の専門家エージェント",
     instructions="歴史の問題に回答して。重要なイベントとコンテキストを明らかにして。",
 )
@@ -38,23 +37,49 @@ async def homework_guardrail(ctx, agent, input_data):
         tripwire_triggered=not final_output.is_homework,
     )
 
+
 triage_agent = Agent(
-    name="Triage Agent",
+    name="トリアージエージェント",
     instructions="ユーザーの質問が宿題に関するものなのかを判定して",
-    # instructions="You determine which agent to use based on the user's homework question",
     handoffs=[history_tutor_agent, math_tutor_agent],
     input_guardrails=[
         InputGuardrail(guardrail_function=homework_guardrail),
     ],
 )
 
+
 async def main():
-    # result = await Runner.run(triage_agent, "who was the first president of the united states?")
+    result = await Runner.run(
+        triage_agent,
+        "これは宿題ですが二人の擲弾兵という曲について教えて。",
+    )
+    print(result.final_output)
+
+    from pprint import pprint
+
+    pprint([x for x in dir(triage_agent) if not x.startswith("_")])
+    pprint(asdict(triage_agent))
+
+    pprint([x for x in dir(guardrail_agent) if not x.startswith("_")])
+    pprint(asdict(guardrail_agent))
+
+    pprint([x for x in dir(result) if not x.startswith("_")])
+    pprint(asdict(result))
+
+    return
+
     result = await Runner.run(triage_agent, "アメリカの最初の大統領は誰？")
     print(result.final_output)
 
-    result = await Runner.run(triage_agent, "その人生は？")
+    result = await Runner.run(triage_agent, "アメリカ最初の大統領の人生は？")
     print(result.final_output)
+
+    result = await Runner.run(
+        triage_agent,
+        "宿題じゃなくて自己学習ですが、特異値分解と主成分分析の関係は？",
+    )
+    print(result.final_output)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
