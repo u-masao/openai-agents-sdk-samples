0592e366c71fe971b5514e5b85639d4ca5e2a53d
u-masao <4973920+u-masao@users.noreply.github.com>
2025-03-24 20:06:18 +0900

    code format

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,9 @@
 from agents import Agent, Runner
 
-agent = Agent(name="Assistant", instructions="You are a helpful assistant")
-result = Runner.run_sync(agent, "今夜の夕飯のメニューを考えて。豚肉と野菜があります。")
+agent = Agent(
+    name="アシスタント", instructions="あなたはとても親切なアシスタントです"
+)
+result = Runner.run_sync(
+    agent, "今夜の夕飯のメニューを考えて。豚肉と野菜があります。"
+)
 print(result.final_output)
